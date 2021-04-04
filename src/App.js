bc76254436fa703de54adafdee48971183e8a993
Parada <paradafaii28@gmail.com>
2021-04-04 09:55:08 -0600

    finsih sign up form and create validate as well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.scss';
 import LoginForm from './Login/LoginForm'
+import SignUpForm from './SignUp/SignUpForm'
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
+      <Switch>
+          <Route path="/" exact component={LoginForm}/>
+          <Route path="/signUp" exact component={SignUpForm}/>
+      </Switch>
     </div>
   );
 }
--- src/SignUp/SignUpForm.js
+++ src/SignUp/SignUpForm.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
-import './LoginForm.scss'
-import SignUpForm from '../SignUp/SignUpForm'
 import { Link } from 'react-router-dom';
 
-const LoginForm = () => {
+
+
+const SignUpForm = () => {
+    const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [passWord, setPassWord] = useState("");
 
     // declare state for handle error
+    const [fullNameErr, setFullNameErr] = useState({});
     const [emailErr, setEmailErr] = useState({});
     const [passWordErr, setPassWordErr] = useState({});
 
@@ -18,13 +20,19 @@ const LoginForm = () => {
     }
 
     const formValidation = () => {
+        const fullNameErr = {}
         const emailErr = {}; 
         const passWordErr = {}; 
         let isValid = true;
 
         // handle error from Full name
+        if(!fullName.trim()){
+            fullNameErr.fullNameRequired = "Full name is required";
+            isValid = false;
+        }
+        // handle error from Full name
         if(!email.trim()){
-            emailErr.firstNameRequired = "Email is required";
+            emailErr.emailRequired = "Email is required";
             isValid = false;
         }
 
@@ -33,6 +41,7 @@ const LoginForm = () => {
             passWordErr.passWordRequired = 'Password is required'
         }
 
+        setFullNameErr(fullNameErr);
         setEmailErr(emailErr);
         setPassWordErr(passWordErr);
         return isValid;
@@ -41,7 +50,20 @@ const LoginForm = () => {
     return (
         <div className="container">
             <form onSubmit={onSubmit} className="form-container">
-                <h1 className='sign-in-title'>Sign-In</h1>
+                <h1 className='sign-in-title'>Sign-Up</h1>
+                <div className="form-inputs">
+                    <label className="from-label">Full Name</label>
+                    <input 
+                        placeholder=""
+                        className="input-box"
+                        type="text"
+                        value={email}
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                    />
+                    {Object.keys(fullNameErr).map((key)=>{
+                        return <div style={{color : "red"}}>{fullNameErr[key]}</div>
+                    })}
+                </div>
                 <div className="form-inputs">
                     <label className="from-label">Email</label>
                     <input 
@@ -68,14 +90,14 @@ const LoginForm = () => {
                         return <div style={{color : "red"}}>{passWordErr[key]}</div>
                     })}
                 </div>
-                <button type="submit" className='sign-in-btn'><p className='sign-in-text'>Sign-In</p></button>
+                <button type="submit" className='sign-in-btn'><p className='sign-in-text'>Sign-Up</p></button>
             </form>
             <div className="not-registered-container">
-                <p>Not registered? </p>
-                <Link to="/signUp" className='sign-up-link'>sign-up</Link>
+                <p>Already registered? </p>
+                <Link to="/" className='sign-up-link'>sign-In</Link>
             </div>
         </div>
     )
 }
 
-export default LoginForm
+export default SignUpForm
