7f5ebf026e1f2b79defa712d4cc3892f84d24195
Parada <paradafaii28@gmail.com>
2021-04-03 18:04:26 -0600

    make validate and message error for fullname and password

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -4,11 +4,39 @@ const LoginForm = () => {
     const [fullName, setFullName] = useState("");
     const [passWord, setPassWord] = useState("");
 
+    // declare state for handle error
+    const [fullNameErr, setFullNameErr] = useState({});
+    const [passWordErr, setPassWordErr] = useState({});
+
+    // create validate 
     const onSubmit = (e) => {
         e.preventDefault();
+        const isValid = formValidation();
     }
+
+    const formValidation = () => {
+        const fullNameErr = {}; 
+        const passWordErr = {}; 
+        let isValid = true;
+
+        // handle error from Full name
+        if(fullName.trim()){
+            fullNameErr.firstNameRequired = "Full name required";
+            isValid = false;
+        }
+
+        //  handle error from Password
+        if(passWord.trim()){
+            passWordErr.passWordRequired = 'Password is required'
+        }
+
+        setFullNameErr(fullNameErr);
+        setPassWordErr(passWordErr);
+        return isValid;
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <label>Full name</label>
             <input 
                 type="text"
@@ -19,7 +47,7 @@ const LoginForm = () => {
             <input 
                 type="password"
                 value={passWord}
-                onChange={(e)=>{setFullName(e.target.value)}}
+                onChange={(e)=>{setPassWord(e.target.value)}}
             />
             <button type="submit">Sign-In</button>
         </form>
