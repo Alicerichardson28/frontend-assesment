89287916925a59b64157159b92fa90fa14e389f5
Parada <paradafaii28@gmail.com>
2021-04-05 11:54:51 -0600

    style welcome and see our product font

--- src/Component/WelcomePage.js
+++ src/Component/WelcomePage.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './WelcomePage.scss'
 
 const WelcomePage = () => {
     return (
-        <div >
+        <div className='welcome-page-container'>
              <div className='content-container'>
                 <h1 className='welcome-title'>Welcome to Apple</h1>
                 <p className='see-product'>see our Product</p>
