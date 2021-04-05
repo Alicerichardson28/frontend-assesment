bfda13ffb90d062164d378d0f1db4e6ff3abe238
Parada <paradafaii28@gmail.com>
2021-04-05 16:26:56 -0600

    fecth data from api and display on shiiping date area

--- src/Component/AllProductPage/Iphone.js
+++ src/Component/AllProductPage/Iphone.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from  'react'
+import PageStructure from '../Template/PageStructure'
+// import Icon from '../Template/Icon'
 import './Iphone.scss'
 
 const Iphone = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch("https://boalt-interview.herokuapp.com/api/shipping-dates")
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(error => console.error(error))
+    },[])
+
+    console.log(data)
     return (
-        <div>
-            Iphone
-        </div>
+        <>
+            <PageStructure/>
+            <div>
+                <h1>The ultimate iPhone</h1>
+                <p>The future is here. Join the iPhone Upgrade Program to get latest iPhone - Now!</p>
+                <p>Starts shipping<h2>{data.iphone}</h2></p>
+            </div>
+        </>
     )
 }
 
--- src/Component/AllProductPage/Laptop.js
+++ src/Component/AllProductPage/Laptop.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import PageStructure from '../Template/PageStructure'
+import Icon from '../Template/Icon'
 import './Laptop.scss'
 
 const Laptop = () => {
     return (
-        <div>
-            Laptop
-        </div>
+        <>
+            <PageStructure/>
+                Laptop
+            <Icon />
+        </>
     )
 }
 
