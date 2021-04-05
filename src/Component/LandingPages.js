4375271b496414fc4dac83d3f1bf38a3126eca72
Parada <paradafaii28@gmail.com>
2021-04-05 12:10:07 -0600

    add aplle icon and install icon

--- src/Component/LandingPages.js
+++ src/Component/LandingPages.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { AiFillApple } from "react-icons/ai";
 import './LandingPages.scss'
 
 
 const LandingPages = () => {
     return (
         <>
-            <div className="nav-bar"></div>
+            <div className="nav-bar"><AiFillApple size='24px' color='white' /></div>
             <div className='landing-page-container'>
                 <div className='content-container'>
                     <Link to='/WelcomePage' className='new-product-link'><h1 className='new-product-title'>New Products Coming This Summer</h1></Link>
