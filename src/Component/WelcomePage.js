171b9cea3e51208d82ffbc19ee8bf78bf2287ca5
Parada <paradafaii28@gmail.com>
2021-04-05 12:36:32 -0600

    add iphone, laptop,applewatch icon and style

--- src/Component/WelcomePage.js
+++ src/Component/WelcomePage.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { IconContext } from "react-icons";
+import { MdPhoneIphone, MdLaptopMac } from "react-icons/md";
+import { CgAppleWatch } from "react-icons/cg";
 import './WelcomePage.scss'
 
 const WelcomePage = () => {
     return (
         <div className='welcome-page-container'>
-             <div className='content-container'>
+            <div className='content-container'>
                 <h1 className='welcome-title'>Welcome to Apple</h1>
-                <p className='see-product'>see our Product</p>
+                <p className='see-product'>see our Products</p>
+                <IconContext.Provider value={{ size: "20px", color: " rgb(173, 173, 173)" }}>
+                    <div className='icons-container'>
+                        <MdPhoneIphone />
+                        <MdLaptopMac />
+                        <CgAppleWatch />
+                    </div>
+                </IconContext.Provider>
            </div>
         </div>
     )
