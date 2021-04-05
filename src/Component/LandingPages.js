238c85e0f037de656674664776d74aebba67999a
Parada <paradafaii28@gmail.com>
2021-04-05 13:01:24 -0600

    resize landing page body

--- src/Component/LandingPages.js
+++ src/Component/LandingPages.js
@@ -7,13 +7,17 @@ import './LandingPages.scss'
 const LandingPages = () => {
     return (
         <>
-            <div className="nav-bar"><AiFillApple size='24px' color='white' /></div>
-            <div className='landing-page-container'>
-                <div className='content-container'>
-                    <Link to='/WelcomePage' className='new-product-link'><h1 className='new-product-title'>New Products Coming This Summer</h1></Link>
-                    <p className='year'>2019</p>
-                </div>
+            <div className="nav-bar-landing-page">
+                <AiFillApple size='32px' color='white' />
             </div>
+            
+                <div className='landing-page-container'>
+                    <div className='content-container'>
+                        <Link to='/WelcomePage' className='new-product-link'><h1 className='new-product-title'>New Products Coming This Summer</h1></Link>
+                        <p className='year'>2019</p>
+                    </div>
+                </div>
+        
         </>
     )
 }
--- src/Component/WelcomePage.js
+++ src/Component/WelcomePage.js
@@ -2,23 +2,29 @@ import React from 'react'
 import { IconContext } from "react-icons";
 import { MdPhoneIphone, MdLaptopMac } from "react-icons/md";
 import { CgAppleWatch } from "react-icons/cg";
+import { AiFillApple } from "react-icons/ai";
 import './WelcomePage.scss'
 
 const WelcomePage = () => {
     return (
-        <div className='welcome-page-container'>
-            <div className='content-container'>
-                <h1 className='welcome-title'>Welcome to Apple</h1>
-                <p className='see-product'>see our Products</p>
-                <IconContext.Provider value={{ size: "20px", color: " rgb(173, 173, 173)" }}>
-                    <div className='icons-container'>
-                        <MdPhoneIphone />
-                        <MdLaptopMac />
-                        <CgAppleWatch />
-                    </div>
-                </IconContext.Provider>
-           </div>
-        </div>
+        <>
+            <div className="nav-bar-welcome-page">
+                <AiFillApple size='32px' color='white' />
+            </div>
+            <div className='welcome-page-container'>
+                <div className='content-container'>
+                    <h1 className='welcome-title'>Welcome to Apple</h1>
+                    <p className='see-product'>See our Products</p>
+                    <IconContext.Provider value={{ size: "20px", color: " rgb(173, 173, 173)" }}>
+                        <div className='icons-container'>
+                            <MdPhoneIphone />
+                            <MdLaptopMac />
+                            <CgAppleWatch />
+                        </div>
+                    </IconContext.Provider>
+            </div>
+            </div>
+        </>
     )
 }
 
