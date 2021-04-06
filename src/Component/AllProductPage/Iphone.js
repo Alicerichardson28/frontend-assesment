759102ffbb39356092b3620a5de8f2dc07e258cc
Parada <paradafaii28@gmail.com>
2021-04-06 12:29:20 -0600

    style second i phone page container

--- src/Component/AllProductPage/Iphone.js
+++ src/Component/AllProductPage/Iphone.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from  'react'
 import PageStructure from '../Template/PageStructure'
 import iPhone from '../../Image/technologies/Iphone 1.png'
+import iPhoneSmallFront from '../../Image/technologies/apple-iphonexs-max-gold.png'
+import iPhoneSmallBack from '../../Image/technologies/apple-iphonexs-max-gold-back-1.png'
 import Icon from '../Template/Icon'
 import './Iphone.scss'
 
@@ -24,9 +26,9 @@ const Iphone = () => {
                         <h2 className='i-phone-h2'>iPhone</h2>
                         <h1 className='i-phone-h1'>The Ultimate</h1>
                         <h1 className='i-phone-h1'>iPhone</h1>
-                        <div className='p-container'>
-                            <p className='i-phone-p'>The future is here. Join the iPhone Upgrade</p>
-                            <p className='i-phone-p'>Program to get latest iPhone - Now!</p>
+                        <div className='top-section-p-container'>
+                            <p className='top-i-phone-p'>The future is here. Join the iPhone Upgrade</p>
+                            <p className='top-i-phone-p'>Program to get latest iPhone - Now!</p>
                         </div>
                         <p className='i-phone-shipping-date'>Starts shipping {data.iphone}</p>
                     </div>
@@ -35,9 +37,15 @@ const Iphone = () => {
                     </div>
                     <Icon />
                 </div>
-                <div>
-                    <p>Form $699</p>
-                    <p>buy Now > </p>
+                <div className='second-section-i-phone-page'>
+                    <div className='second-section-p-container'>
+                        <p className='second-i-phone-p'>From $699</p>
+                        <p className='i-phone-buy-now'>Buy Now > </p>
+                    </div>
+                    <div className='second-section-img-container'>
+                        <img alt='front i phone small' className='img-second-section' src={iPhoneSmallFront}/>
+                        <img alt='back i phone small' className='img-second-section' src={iPhoneSmallBack}/>
+                    </div>
                 </div>
             </div>
         </>
